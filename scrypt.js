b47507ded0743eca9e9a93aece40eb277efef466
Paul Nwodo <145398513+Tonypaul544@users.noreply.github.com>
2025-01-23 22:01:53 +0100

    Add files via upload

--- scrypt.js
+++ scrypt.js
@@ -6,7 +6,6 @@ let cat = document.querySelector(".cat");
 let Rabit = document.querySelector(".Rabit");
 let monkey = document.querySelector(".monkey");
 
-
 dog.addEventListener("click", dogBg);
 function dogBg() {
   bodyBg.style.backgroundImage = "url('images/dog face.webp')";
@@ -32,9 +31,36 @@ let email = document.getElementById("emailInput");
 let comment = document.getElementById("commentInput");
 // let name = document.getElementById("nameInput");
 
-// let submitBtn = document.querySelector(".submitBtn");
+let submitBtn = document.querySelector(".submitBtn");
 
 // submitBtn.addEventListener("click", submitwarning);
 // function submitwarning() {
 //     alert("fill the form");
 // }
+
+
+
+let dog2 = document.querySelector(".Dog2");
+let cat2 = document.querySelector(".Cat2");
+let Rabit2 = document.querySelector(".Rabit2");
+let monkey2 = document.querySelector(".Monkey2");
+
+dog2.addEventListener("click", dogBg2);
+function dogBg2() {
+  bodyBg.style.backgroundImage = "url('images/dog face.webp')";
+}
+
+cat2.addEventListener("click", catBg2);
+function catBg2() {
+  bodyBg.style.backgroundImage = "url('images/cat face 2.jpeg')";
+}
+
+Rabit2.addEventListener("click", RabitBg2);
+function RabitBg2() {
+  bodyBg.style.backgroundImage = "url('images/Rabit face.jpeg')";
+}
+
+monkey2.addEventListener("click", monkeyBg2);
+function monkeyBg2() {
+  bodyBg.style.backgroundImage = "url('images/monkey.jpeg')";
+}
